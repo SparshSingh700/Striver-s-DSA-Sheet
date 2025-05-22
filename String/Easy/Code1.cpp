576521bdb893b8a25f678bdee359a5db33be31ae
SparshSingh700 <148028935+SparshSingh700@users.noreply.github.com>
2025-05-22 22:07:09 +0530

    Remove Outermost Parentheses

---
+++ String/Easy/Code1.cpp
@@ -0,0 +1,23 @@
+//Leetcode 1021. Remove Outermost Parentheses
+class Solution {
+    public String removeOuterParentheses(String s) {
+       StringBuilder S=new StringBuilder();
+       int c=0;
+       for(int i=0; i<s.length(); i++){
+        if(s.charAt(i)=='('){
+            if(c==0) c++;
+            else{
+                S=S.append("(");
+                c++;
+            }
+        }
+        else{
+            if(c<=1) c--;
+            else{
+                S=S.append(")");
+                c--;
+            }
+        }
+       } return S.toString(); 
+    }
+}
